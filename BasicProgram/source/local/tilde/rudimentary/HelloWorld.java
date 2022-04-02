6e6cf0c97bc980f8127b087250c6e000b09745ea
mrityunjay9693 <mkaykumar123@gmail.com>
2022-04-02 12:38:33 +0530

    hello world updates

--- BasicProgram/source/local/tilde/rudimentary/HelloWorld.java
+++ BasicProgram/source/local/tilde/rudimentary/HelloWorld.java
@@ -1,5 +1,8 @@
 package BasicProgram.source.local.tilde.rudimentary;
 
 public class HelloWorld {
-    System.out.println("Hello World");
+    
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
