ea4a4b71381ab4093e8e774c235380bc3f628066
mrityunjay9693 <mkaykumar123@gmail.com>
2022-04-08 05:17:08 +0530

    basic program

--- BasicProgram/source/local/tilde/rudimentary/HelloWorld.java
+++ BasicProgram/source/local/tilde/rudimentary/HelloWorld.java
@@ -1,8 +1,13 @@
 package BasicProgram.source.local.tilde.rudimentary;
 
 public class HelloWorld {
-    
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        //System.out.println("Hello World");
+        greet();
+    }
+
+    public static void greet() {
+        System.out.println("Hello World");
     }
 }
