42b1fe1d6ab3f146b63186d99717eae3821f24aa
mrityunjay9693 <mkaykumar123@gmail.com>
2022-04-10 14:22:20 +0530

    program skeleton is designed

---
+++ src/local/tilde/oops/BankApplication.java
@@ -0,0 +1,47 @@
+package local.tilde.oops;
+
+public class BankApplication {
+    private int acNo;
+    private String name;
+    private String acType;
+    private float balance;
+
+    public static void menu()
+    {
+
+    }
+
+    public void newAccount()
+    {
+
+    }
+
+    public void editAccount()
+    {
+
+    }
+
+    public void printAccount()
+    {
+
+    }
+
+    public void deposit()
+    {
+
+    }
+
+    public void withdraw()
+    {
+
+    }
+
+    public void displayBalance()
+    {
+
+    }
+
+    public static void main(String[] args) {
+        menu();
+    }   
+}
