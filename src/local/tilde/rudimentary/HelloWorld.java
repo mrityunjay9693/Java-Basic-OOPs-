1e07d7858a0488e2d54576ea9d691cc7a1c070bd
mrityunjay9693 <mkaykumar123@gmail.com>
2022-04-10 21:10:07 +0530

    programs

--- src/local/tilde/rudimentary/HelloWorld.java
+++ src/local/tilde/rudimentary/HelloWorld.java
@@ -3,5 +3,10 @@ package local.tilde.rudimentary;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-    }   
+        greet();
+    }
+
+    public static void greet() {
+        System.out.println("greet");
+    }
 }
