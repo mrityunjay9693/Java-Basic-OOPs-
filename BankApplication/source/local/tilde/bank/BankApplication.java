b03f4467a2c1448223343b8f2748a7f0d26a83c5
Mrityunjay Kumar <mkaykumar123@gmail.com>
2023-10-03 04:56:38 +0530

    Delete BankApplication/source/local/tilde/bank directory

--- BankApplication/source/local/tilde/bank/BankApplication.java
+++
@@ -1,9 +0,0 @@
-package BankApplication.source.local.tilde.bank;
-
-public class BankApplication {
-    
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-    
-}
