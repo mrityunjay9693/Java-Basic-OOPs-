9b2102beb19cc9ca05a397328998a35ba267c7d3
mrityunjay9693 <mkaykumar123@gmail.com>
2023-12-05 10:34:25 +0530

    hello world program

---
+++ src/local/tilde/basic/HelloWorld.java
@@ -0,0 +1,13 @@
+package local.tilde.basic;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        
+        System.out.println("Hello World!");
+        System.out.println("Welcome to tilde~");
+        System.out.print("This is Java programming language and ");
+        System.out.println("Java's technology journey....");
+    
+    }
+    
+}
