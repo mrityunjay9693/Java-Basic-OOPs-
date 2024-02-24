57903924afc40067c2df19641fc811b7168284ba
mrityunjay9693 <mkaykumar123@gmail.com>
2024-02-24 17:12:06 +0530

    checkAccount() and deposit()

--- src/local/tilde/oops/BankAccount.java
+++ src/local/tilde/oops/BankAccount.java
@@ -21,6 +21,7 @@ public class BankAccount {
             System.out.println("MAIN MENU");
             System.out.println("1. New Account");
             System.out.println("2. Deposit Amount");
+            System.out.println("3. Withdraw");
             System.out.println("4. Print Account");
             System.out.print("Enter choice(0 to exit): ");
             ch = scan.nextInt();
@@ -33,6 +34,9 @@ public class BankAccount {
                     break;
                 case 2:
                     deposit();
+                    break;
+                case 3:
+                    withdraw();
                     break;
                 case 4:
                     printAccount();
@@ -67,59 +71,78 @@ public class BankAccount {
     }
 
     // Parameterized constructor.
-    public void initializeAccount(int acNo, String name, String acType, float balance) {
-        this.acNo = acNo;
-        this.name = name;
-        this.acType = acType;
-        this.balance = balance;
-        System.out.println("\nAccount initialized!\n");
-    }
+    // public void initializeAccount(int acNo, String name, String acType, float
+    // balance) {
+    // this.acNo = acNo;
+    // this.name = name;
+    // this.acType = acType;
+    // this.balance = balance;
+    // System.out.println("\nAccount initialized!\n");
+    // }
 
     public void editAccount() {
 
     }
 
-    public void printAccount() {
-        int readStatus = 0;
+    public int checkAccount() {
+        int status = 0;
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter Account Number : ");
+        System.out.print("\nEnter account number: ");
         acNo = scan.nextInt();
-        String sql = "SELECT * FROM account WHERE acno = " + Integer.toString(acNo);
+        String sql = "SELECT * FROM account WHERE acno=" + acNo;
         try {
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost/bank_2022", "bank_22", "BanK$2022");
             Statement stmt = con.createStatement();
-            ResultSet result = stmt.executeQuery(sql); // ResultSet : Interface and result is its reference
+            ResultSet result = stmt.executeQuery(sql);
             if (result.next()) {
-                readStatus = 1;
-                acNo = result.getInt("acno");
+                status = 1;
                 name = result.getString("name");
                 acType = result.getString("actype");
                 balance = result.getFloat("balance");
             }
-        } catch (SQLException ex) {
-            System.out.print(ex.toString());
+        } catch (Exception ex) {
+            // TODO: handle exception
+            System.out.println(ex.toString());
         }
-        if(readStatus==1){
+        return status;
+    }
+
+    public void printAccount() {
+        if (checkAccount() == 1) {
             System.out.println("\nAccount Number:" + acNo);
             System.out.println("Account Holder Name: " + name);
             System.out.println("Account Type: " + acType);
             System.out.println("Account Balance: " + balance + "\n");
-        } else{
-            System.out.println("Account not found!");
+        } else {
+            System.out.println("\nAccount not found!");
         }
         System.out.println();
     }
 
     public void deposit() {
-        if (init_flag == 0) {
-            float amount;
+        if (checkAccount() == 1) {
             Scanner scan = new Scanner(System.in);
+            System.out.println("Name: " + name);
+            System.out.print("Continue? (y/n):");
+            String continuee = scan.next();
+            if(continuee.equals("n")){
+                return;
+            }
             System.out.print("Enter amount: ");
-            amount = scan.nextFloat();
-            balance += amount;
-            System.out.println("\nAmount deposited\n");
+            float amount = scan.nextFloat();
+            balance += amount; // amount is deposited to baance
+            try{
+                Connection con = DriverManager.getConnection("jdbc:mysql://localhost/bank_2022", "bank_22", "BanK$2022");
+                Statement stmt = con.createStatement();
+                String sql = "UPDATE account SET balance=" + balance;
+                stmt.executeUpdate(sql);
+            } catch(SQLException ex){
+                System.out.print(ex.toString());
+            }
+            
+            System.out.println("\nAmount Deposited\n");
         } else {
-            System.out.println("\nAccount is not initialized.\n");
+            System.out.println("\nAccount Not Found!\n");
         }
     }
 
